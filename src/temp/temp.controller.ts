88b84317501bbb2ab6c8f66f9284a506e025b551
hjyang <hjyang@cookapps.com>
2022-11-22 23:23:00 +0900

    feat: temp api 수정

--- src/temp/temp.controller.ts
+++ src/temp/temp.controller.ts
@@ -6,36 +6,51 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from "@nestjs/common";
 import { TempService } from "./temp.service";
 import { CreateTempDto } from "./dto/create-temp.dto";
 import { UpdateTempDto } from "./dto/update-temp.dto";
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth()
 @Controller("temp")
+@ApiTags("임시 API")
 export class TempController {
   constructor(private readonly tempService: TempService) {}
 
   @Post()
-  create(@Body() createTempDto: CreateTempDto) {
-    return this.tempService.create(createTempDto);
-  }
-  @Post()
-  upsert(@Body() createTempDto: CreateTempDto) {
-    return this.tempService.upsert(createTempDto);
+  @ApiOperation({
+    summary: "임시 데이터 저장",
+  })
+  @ApiBody({
+    type: CreateTempDto,
+    examples: {
+      1: {
+        summary: "임시 데이터",
+        description:
+          "type은 제약조건 없음, data은 json serialize하여 string 으로 전송 ",
+        value: {
+          uid: 1,
+          type: "userData",
+          data: '{ "a": 1, "b": 2, "c": 3}',
+        } as CreateTempDto,
+      },
+    },
+  })
+  async upsert(@Body() createTempDto: CreateTempDto) {
+    return await this.tempService.upsert(createTempDto);
   }
 
-  @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.tempService.findOne(+id);
-  }
+  @Get(":uid/:type")
+  @ApiOperation({
+    summary: "임시 데이터 조회",
+  })
+  async findOne(@Param("uid") uid: number, @Param("type") type: string) {
+    const temp = await this.tempService.findOne(uid, type);
 
-  @Patch(":id")
-  update(@Param("id") id: string, @Body() updateTempDto: UpdateTempDto) {
-    return this.tempService.update(+id, updateTempDto);
-  }
-
-  @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.tempService.remove(+id);
+    return JSON.parse(temp.data);
   }
 }
--- src/temp/temp.service.ts
+++ src/temp/temp.service.ts
@@ -9,22 +9,14 @@ import { Temp } from "./entities/temp.entity";
 export class TempService {
   constructor(@InjectRepository(Temp) private tempRepo: Repository<Temp>) {}
 
-  create(createTempDto: CreateTempDto) {
+  upsert(createTempDto: CreateTempDto) {
     return this.tempRepo.save(createTempDto);
   }
-  upsert(createTempDto: CreateTempDto) {
-    return this.tempRepo.upsert(createTempDto, ["uid", "type"]);
-  }
+  // upsert(createTempDto: CreateTempDto) {
+  //   return this.tempRepo.upsert(createTempDto, ["uid", "type"]);
+  // }
 
-  findOne(id: number) {
-    return `This action returns a #${id} temp`;
-  }
-
-  update(id: number, updateTempDto: UpdateTempDto) {
-    return `This action updates a #${id} temp`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} temp`;
+  findOne(uid: number, type: string) {
+    return this.tempRepo.findOne({ where: { uid, type } });
   }
 }
