bf19b0dedb0c1dbab84d6d689cfc2ee3ce695792
hjyang <hjyang@cookapps.com>
2022-11-23 16:53:25 +0900

    feat: doc id -> uid

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,24 +8,24 @@ import {
   Delete,
   UseGuards,
   Req,
-} from '@nestjs/common';
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
+} from "@nestjs/common";
+import { UserService } from "./user.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+import { ApiTags, ApiOperation, ApiBearerAuth } from "@nestjs/swagger";
 
 @UseGuards(JwtAuthGuard)
 @ApiBearerAuth()
-@Controller('user')
-@ApiTags('유저 API')
+@Controller("user")
+@ApiTags("유저 API")
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
   @ApiOperation({
-    summary: '유저생성',
-    description: '유저를 생성한다.',
+    summary: "유저생성",
+    description: "유저를 생성한다.",
   })
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
@@ -33,8 +33,8 @@ export class UserController {
 
   @Get()
   @ApiOperation({
-    summary: '유저 조회(전체)',
-    description: '전체유저를 조회한다.',
+    summary: "유저 조회(전체)",
+    description: "전체유저를 조회한다.",
   })
   findAll(@Req() req) {
     console.log(req.user);
@@ -42,30 +42,30 @@ export class UserController {
     return this.userService.findAll();
   }
 
-  @Get(':uid')
+  @Get(":uid")
   @ApiOperation({
-    summary: '유저 조회(단일)',
-    description: '단일유저를 조회한다.',
+    summary: "유저 조회(단일)",
+    description: "단일유저를 조회한다.",
   })
-  findOne(@Param('uid') uid: number) {
+  findOne(@Param("uid") uid: number) {
     return this.userService.findOne({ uid });
   }
 
-  @Patch(':id')
+  @Patch(":uid")
   @ApiOperation({
-    summary: '유저 수정',
-    description: '유저를 수정한다.',
+    summary: "유저 수정",
+    description: "유저를 수정한다.",
   })
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+  update(@Param("uid") uid: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+uid, updateUserDto);
   }
 
-  @Delete(':id')
+  @Delete(":uid")
   @ApiOperation({
-    summary: '유저 삭제',
-    description: '유저를 삭제한다.',
+    summary: "유저 삭제",
+    description: "유저를 삭제한다.",
   })
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  remove(@Param("uid") uid: string) {
+    return this.userService.remove(+uid);
   }
 }
