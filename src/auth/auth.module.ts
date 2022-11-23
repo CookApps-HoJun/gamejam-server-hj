3c1d51ec5959c103c2672bae4601b50f13ed5b18
hjyang <hjyang@cookapps.com>
2022-11-23 16:56:17 +0900

    chore:  jwt 24시간 으로

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UserModule } from '../user/user.module';
-import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './local.strategy';
-import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './jwt.strategy';
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { UserModule } from "../user/user.module";
+import { PassportModule } from "@nestjs/passport";
+import { LocalStrategy } from "./local.strategy";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
     UserModule,
     PassportModule,
     JwtModule.register({
-      secret: 'secretKey',
-      signOptions: { expiresIn: '6000s' },
+      secret: "secretKey",
+      signOptions: { expiresIn: `${60 * 60 * 24}s` },
     }),
   ],
   controllers: [AuthController],
