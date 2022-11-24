5f33d3d861d09382321606a743e70061e27cea2e
hjyang <hjyang@cookapps.com>
2022-11-24 22:42:37 +0900

    feat: http swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,23 @@ import { SwaggerModule } from '@nestjs/swagger';
 import { BaseAPIDocument } from './document/swagger.document';
 import helmet from 'helmet';
 
+function disableUpgradeInsecureRequests(app, helmet) {
+  const defaultDirectives = helmet.contentSecurityPolicy.getDefaultDirectives();
+  delete defaultDirectives['upgrade-insecure-requests'];
+
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        ...defaultDirectives,
+      },
+    }),
+  );
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  disableUpgradeInsecureRequests(app, helmet);
 
   const config = new BaseAPIDocument().initializeOptions();
   const document = SwaggerModule.createDocument(app, config);
